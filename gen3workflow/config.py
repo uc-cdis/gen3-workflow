74039d4e3cd27d8c047e01d40455640864ffe48a
Sai Shanmukha <nss10@outlook.com>
2024-11-13 14:52:56 -0600

    Change the name of the "JOB_IMAGES" config variable to "WHITELISTED_REPO_LIST

--- gen3workflow/config.py
+++ gen3workflow/config.py
@@ -40,7 +40,7 @@ class Gen3WorkflowConfig(Config):
                 "MAX_IAM_KEYS_PER_USER": {"type": "integer", "maximum": 100},
                 "IAM_KEYS_LIFETIME_DAYS": {"type": "integer"},
                 "ARBORIST_URL": {"type": ["string", "null"]},
-                "JOB_IMAGE": {"type": "array", "items": {"type": "string"}},
+                "WHITELISTED_REPO_LIST": {"type": "array", "items": {"type": "string"}},
                 "TES_SERVER_URL": {"type": "string"},
             },
         }
