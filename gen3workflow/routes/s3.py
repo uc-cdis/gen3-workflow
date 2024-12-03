3fa21ad8222677b759b1b52faec047115ea2166b
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2024-12-03 14:45:18 -0600

    wip

--- gen3workflow/routes/s3.py
+++ gen3workflow/routes/s3.py
@@ -2,52 +2,26 @@ from datetime import datetime
 import hashlib
 import json
 import os
-import logging
 import urllib.parse
-import uuid
 
 from botocore.awsrequest import AWSRequest
+from fastapi import APIRouter, Request
 from botocore.credentials import Credentials
-from botocore.auth import SigV4Auth
-import boto3
 import hmac
-
-from fastapi import APIRouter, FastAPI, Request
 import httpx
-# import requests # TODO replace with httpx
-import uvicorn
+from starlette.responses import Response
 
-from starlette.responses import JSONResponse, Response, StreamingResponse
-from io import BytesIO
+from gen3workflow import logger
+
 
 # TODO Generate a presigned URL if the request is a GET request, see https://cdis.slack.com/archives/D01DMJWKVB5/p1733169741227879
 
-
-# The base URL to forward requests to
-bucket = "ga4ghtes-pauline-planx-pla-net"
-FORWARD_TO_BASE_URL = f"https://{bucket}.s3.amazonaws.com"
-
-
-# app = FastAPI()
-
-
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(message)s',
-    handlers=[
-        logging.FileHandler('server.log'),
-        logging.StreamHandler()
-    ]
-)
 
 router = APIRouter(prefix="/s3")
 
 
 async def _log_request(request, path):
-    # Get current timestamp
-    timestamp = datetime.now().isoformat()
-    
-    # Reading body as bytes, then decode it as string if necessary
+    # Read body as bytes, then decode it as string if necessary
     body_bytes = await request.body()
     try:
         body = body_bytes.decode()
@@ -57,8 +31,8 @@ async def _log_request(request, path):
         body = json.loads(body)
     except:
         pass  # Keep body as string if not JSON
-        
-    # Create log entry
+
+    timestamp = datetime.now().isoformat()
     log_entry = {
         'timestamp': timestamp,
         'method': request.method,
@@ -66,9 +40,7 @@ async def _log_request(request, path):
         'headers': dict(request.headers),
         'body': body,
     }
-    
-    # Log as JSON for easier parsing
-    logging.info(json.dumps(log_entry, indent=2))
+    logger.debug(f"Incoming request: {json.dumps(log_entry, indent=2)}")
 
 
 def get_signature_key(key, date_stamp, region_name, service_name):
@@ -89,40 +61,60 @@ def get_signature_key(key, date_stamp, region_name, service_name):
 async def catch_all_v4(path: str, request: Request):
     if "KEY" not in os.environ:
         raise Exception("No key")
-    await _log_request(request, path)
-    query_params = dict(request.query_params)
+
+    # await _log_request(request, path)
+
+    # TODO get bucket from path
+    bucket = "ga4ghtes-pauline-planx-pla-net"
+
     def url_enc(s):
-        return urllib.parse.quote_plus(s) #, safe='/', encoding=None, errors=None)
+        return urllib.parse.quote_plus(s)
+
+    query_params = dict(request.query_params)
     query_params_names = sorted(list(query_params.keys())) # query params have to be sorted
     canonical_r_q_params = "&".join(f"{url_enc(key)}={url_enc(query_params[key])}" for key in query_params_names)
 
-    used_path = path.split(bucket)[1]
-    used_url = "/".join(used_path.split("/")[1:])
+    # Example 1:
+    # - path = my-bucket//
+    # - request_path = //
+    # - api_endpoint = /
+    # Example 2:
+    # - path = my-bucket/pre/fix/
+    # - request_path = /pre/fix/
+    # - api_endpoint = pre/fix/
+    request_path = path.split(bucket)[1]
+    api_endpoint = "/".join(request_path.split("/")[1:])
 
-    headers = {}
-    # TODO try again to include all the headers
     # headers = dict(request.headers)
     # headers.pop("authorization")
-
-    url = f"{FORWARD_TO_BASE_URL}/{used_url}"
-    body = await request.body()
+    headers = {}
+    # TODO try again to include all the headers
+    # `x-amz-content-sha256` is sometimes set to "STREAMING-AWS4-HMAC-SHA256-PAYLOAD" in
+    # the original request, but i was not able to get the signing working when copying it
 
     # if "Content-Type" in request.headers:
     #     headers["content-type"] = request.headers["Content-Type"]
     headers['host'] = f'{bucket}.s3.amazonaws.com'
 
     # Hash the request body
+    body = await request.body()
     body_hash = hashlib.sha256(body).hexdigest()
     headers['x-amz-content-sha256'] = body_hash
+    # headers['x-amz-content-sha256'] = request.headers['x-amz-content-sha256']
+    # if 'content-length' in request.headers:
+    #     headers['content-length'] = request.headers['content-length']
+    # if 'x-amz-decoded-content-length' in request.headers:
+    #     headers['x-amz-decoded-content-length'] = request.headers['x-amz-decoded-content-length']
     
     # Ensure 'x-amz-date' is included in the headers (it's needed for signature calculation)
     amz_date = datetime.utcnow().strftime('%Y%m%dT%H%M%SZ')
     headers['x-amz-date'] = amz_date
 
     # AWS request object
+    aws_api_url = f"https://{bucket}.s3.amazonaws.com/{api_endpoint}"
     aws_request = AWSRequest(
         method=request.method,
-        url=url,
+        url=aws_api_url,
         data=body,
         headers=headers
     )
@@ -133,14 +125,14 @@ async def catch_all_v4(path: str, request: Request):
     # Construct the canonical request (with cleaned-up path)
     canonical_request = (
         f"{request.method}\n"
-        f"{used_path}\n"
+        f"{request_path}\n"
         f"{canonical_r_q_params}\n"  # Query parameters
         f"{canon_headers}"
         f"\n"
         f"{header_names}\n"  # Signed headers
         f"{headers['x-amz-content-sha256']}"  # Final Body hash
     )
-    logging.info(f"- Canonical Request:\n{canonical_request}")
+    logger.debug(f"- Canonical Request:\n{canonical_request}")
 
     # AWS Credentials for signing
     credentials = Credentials(
@@ -158,37 +150,32 @@ async def catch_all_v4(path: str, request: Request):
         f"{date_stamp}/{region}/{service}/aws4_request\n"  # Credential scope
         f"{hashlib.sha256(canonical_request.encode('utf-8')).hexdigest()}"  # Hash of the Canonical Request
     )
-
-    # Log String to Sign
-    logging.info(f"- String to Sign:\n{string_to_sign}")
+    logger.debug(f"- String to Sign:\n{string_to_sign}")
 
     # Generate the signing key using our `get_signature_key` function
     signing_key = get_signature_key(credentials.secret_key, date_stamp, region, service)
 
     # Calculate the signature by signing the string to sign with the signing key
     signature = hmac.new(signing_key, string_to_sign.encode('utf-8'), hashlib.sha256).hexdigest()
-
-    # Log Signature
-    logging.info(f"- Signature: {signature}")
+    logger.debug(f"- Signature: {signature}")
 
     # Ensure all headers that are in the request are included in the SignedHeaders
     signed_headers = ';'.join(sorted([k.lower() for k in headers.keys() if k != 'authorization']))
 
     # Log final headers before sending the request
     aws_request.headers['Authorization'] = f"AWS4-HMAC-SHA256 Credential={credentials.access_key}/{date_stamp}/{region}/{service}/aws4_request, SignedHeaders={signed_headers}, Signature={signature}"
-
-    logging.info(f"- Signed Headers:\n{aws_request.headers}")
+    logger.debug(f"- Signed Headers:\n{aws_request.headers}")
 
     # Send the signed request to S3
     prepared_request = aws_request.prepare()
-    logging.info(f"- Making {prepared_request.method} request to {prepared_request.url}")
+    logger.debug(f"- Making {prepared_request.method} request to {prepared_request.url}")
 
     # Perform the actual HTTP request
     async with httpx.AsyncClient() as client:
         # print("request:", {"method": request.method, "url": url, "body": body, "headers": prepared_request.headers, "query param": query_params})
         response = await client.request(
             method=request.method,
-            url=url,
+            url=aws_api_url,
             headers=prepared_request.headers,
             params=query_params,
             data=body,
@@ -196,7 +183,7 @@ async def catch_all_v4(path: str, request: Request):
 
     # Check for errors
     if response.status_code != 200:
-        logging.error(f"- Error from AWS: {response}")
+        logger.error(f"- Error from AWS: {response}")
 
     if "Content-Type" in response.headers:
         return Response(content=response.content, status_code=response.status_code, media_type=response.headers['Content-Type'])
