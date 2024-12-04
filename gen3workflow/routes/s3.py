7ea2a352d5b0cbc27ea264e2b4b63a50f546b56c
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2024-12-04 16:47:51 -0600

    working version

--- gen3workflow/routes/s3.py
+++ gen3workflow/routes/s3.py
@@ -4,17 +4,19 @@ import json
 import os
 import urllib.parse
 
-from botocore.awsrequest import AWSRequest
 from fastapi import APIRouter, Request
+from fastapi.security import HTTPAuthorizationCredentials
 from botocore.credentials import Credentials
 import hmac
 import httpx
+from starlette.datastructures import Headers
 from starlette.responses import Response
 
-from gen3workflow import logger
+from gen3workflow import aws_utils, logger
+from gen3workflow.auth import Auth
 
 
-# TODO Generate a presigned URL if the request is a GET request, see https://cdis.slack.com/archives/D01DMJWKVB5/p1733169741227879
+# TODO Generate a presigned URL if the request is a GET request, see https://cdis.slack.com/archives/D01DMJWKVB5/p1733169741227879 - is that required?
 
 
 router = APIRouter(prefix="/s3")
@@ -43,7 +45,25 @@ async def _log_request(request, path):
     logger.debug(f"Incoming request: {json.dumps(log_entry, indent=2)}")
 
 
-def get_signature_key(key, date_stamp, region_name, service_name):
+def get_access_token(headers: Headers) -> str:
+    """
+    Extract the user's access token, which should have been provided as the key ID, from the
+    Authorization header in the following expected format:
+    `AWS4-HMAC-SHA256 Credential=<key ID>/<date>/<region>/<service>/aws4_request, SignedHeaders=<>, Signature=<>`
+
+    Args:
+        headers (Headers): request headers
+
+    Returns:
+        str: the user's access token or "" if not found
+    """
+    auth_header = headers.get("authorization")
+    if not auth_header:
+        return ""
+    return auth_header.split("Credential=")[1].split("/")[0]
+
+
+def get_signature_key(key: str, date_stamp: str, region_name: str, service_name: str) -> str:
     """
     Create a signing key using the AWS Signature Version 4 algorithm.
     """
@@ -59,20 +79,35 @@ def get_signature_key(key, date_stamp, region_name, service_name):
     methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "TRACE", "HEAD"]
 )
 async def catch_all_v4(path: str, request: Request):
+    """TODO
+
+    Args:
+        path (str): _description_
+        request (Request): _description_
+
+    Raises:
+        Exception: _description_
+
+    Returns:
+        _type_: _description_
+    """
     if "KEY" not in os.environ:
         raise Exception("No key")
 
     # await _log_request(request, path)
 
-    # TODO get bucket from path
-    bucket = "ga4ghtes-pauline-planx-pla-net"
-
-    def url_enc(s):
-        return urllib.parse.quote_plus(s)
+    # extract the user's access token from the request headers, and use it to get the name of
+    # the user's bucket
+    auth = Auth(api_request=request)
+    auth.bearer_token = HTTPAuthorizationCredentials(scheme="bearer", credentials=get_access_token(request.headers))
+    token_claims = await auth.get_token_claims()
+    user_id = token_claims.get("sub")
+    user_bucket = aws_utils.get_safe_name_from_user_id(user_id)
+    user_bucket = "ga4ghtes-pauline-planx-pla-net" # TODO remove - for testing
 
     query_params = dict(request.query_params)
     query_params_names = sorted(list(query_params.keys())) # query params have to be sorted
-    canonical_r_q_params = "&".join(f"{url_enc(key)}={url_enc(query_params[key])}" for key in query_params_names)
+    canonical_r_q_params = "&".join(f"{urllib.parse.quote_plus(key)}={urllib.parse.quote_plus(query_params[key])}" for key in query_params_names)
 
     # Example 1:
     # - path = my-bucket//
@@ -82,7 +117,7 @@ async def catch_all_v4(path: str, request: Request):
     # - path = my-bucket/pre/fix/
     # - request_path = /pre/fix/
     # - api_endpoint = pre/fix/
-    request_path = path.split(bucket)[1]
+    request_path = path.split(user_bucket)[1]
     api_endpoint = "/".join(request_path.split("/")[1:])
 
     # headers = dict(request.headers)
@@ -94,7 +129,7 @@ async def catch_all_v4(path: str, request: Request):
 
     # if "Content-Type" in request.headers:
     #     headers["content-type"] = request.headers["Content-Type"]
-    headers['host'] = f'{bucket}.s3.amazonaws.com'
+    headers['host'] = f'{user_bucket}.s3.amazonaws.com'
 
     # Hash the request body
     body = await request.body()
@@ -110,15 +145,6 @@ async def catch_all_v4(path: str, request: Request):
     amz_date = datetime.utcnow().strftime('%Y%m%dT%H%M%SZ')
     headers['x-amz-date'] = amz_date
 
-    # AWS request object
-    aws_api_url = f"https://{bucket}.s3.amazonaws.com/{api_endpoint}"
-    aws_request = AWSRequest(
-        method=request.method,
-        url=aws_api_url,
-        data=body,
-        headers=headers
-    )
-
     canon_headers = "".join(f"{key}:{headers[key]}\n" for key in sorted(list(headers.keys())))
     header_names = ";".join(sorted(list(headers.keys())))
 
@@ -132,9 +158,10 @@ async def catch_all_v4(path: str, request: Request):
         f"{header_names}\n"  # Signed headers
         f"{headers['x-amz-content-sha256']}"  # Final Body hash
     )
-    logger.debug(f"- Canonical Request:\n{canonical_request}")
+    # logger.debug(f"- Canonical Request:\n{canonical_request}")
 
     # AWS Credentials for signing
+    # TODO support either AWS IAM key or service account
     credentials = Credentials(
         access_key=os.environ.get('KEY'),
         secret_key=os.environ.get('SECRET')
@@ -150,40 +177,35 @@ async def catch_all_v4(path: str, request: Request):
         f"{date_stamp}/{region}/{service}/aws4_request\n"  # Credential scope
         f"{hashlib.sha256(canonical_request.encode('utf-8')).hexdigest()}"  # Hash of the Canonical Request
     )
-    logger.debug(f"- String to Sign:\n{string_to_sign}")
+    # logger.debug(f"- String to Sign:\n{string_to_sign}")
 
     # Generate the signing key using our `get_signature_key` function
     signing_key = get_signature_key(credentials.secret_key, date_stamp, region, service)
 
     # Calculate the signature by signing the string to sign with the signing key
     signature = hmac.new(signing_key, string_to_sign.encode('utf-8'), hashlib.sha256).hexdigest()
-    logger.debug(f"- Signature: {signature}")
+    # logger.debug(f"- Signature: {signature}")
 
     # Ensure all headers that are in the request are included in the SignedHeaders
     signed_headers = ';'.join(sorted([k.lower() for k in headers.keys() if k != 'authorization']))
 
     # Log final headers before sending the request
-    aws_request.headers['Authorization'] = f"AWS4-HMAC-SHA256 Credential={credentials.access_key}/{date_stamp}/{region}/{service}/aws4_request, SignedHeaders={signed_headers}, Signature={signature}"
-    logger.debug(f"- Signed Headers:\n{aws_request.headers}")
-
-    # Send the signed request to S3
-    prepared_request = aws_request.prepare()
-    logger.debug(f"- Making {prepared_request.method} request to {prepared_request.url}")
+    headers['authorization'] = f"AWS4-HMAC-SHA256 Credential={credentials.access_key}/{date_stamp}/{region}/{service}/aws4_request, SignedHeaders={signed_headers}, Signature={signature}"
+    # logger.debug(f"- Signed Headers:\n{aws_request.headers}")
 
     # Perform the actual HTTP request
+    s3_api_url = f"https://{user_bucket}.s3.amazonaws.com/{api_endpoint}"
+    # logger.debug(f"- Making {request.method} request to {s3_api_url}")
     async with httpx.AsyncClient() as client:
-        # print("request:", {"method": request.method, "url": url, "body": body, "headers": prepared_request.headers, "query param": query_params})
         response = await client.request(
             method=request.method,
-            url=aws_api_url,
-            headers=prepared_request.headers,
+            url=s3_api_url,
+            headers=headers,
             params=query_params,
             data=body,
         )
-
-    # Check for errors
     if response.status_code != 200:
-        logger.error(f"- Error from AWS: {response}")
+        logger.error(f"Error from AWS: {response.status_code} {response.text}")
 
     if "Content-Type" in response.headers:
         return Response(content=response.content, status_code=response.status_code, media_type=response.headers['Content-Type'])
