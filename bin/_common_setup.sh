b3004eb0643f00ae5902fbf909ec80374b37b2e3
Sai Shanmukha <nss10@outlook.com>
2025-04-15 17:48:10 -0500

    Update _common_setup setup_prometheus shell scripts

--- bin/_common_setup.sh
+++ bin/_common_setup.sh
@@ -6,8 +6,14 @@ set -e
 
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+if [ -f "/src/gen3-workflow-config.yaml" ]; then
+  echo "Loading environment variables from ${CURRENT_DIR}/.env"
+  PROMETHEUS_MULTIPROC_DIR=$(grep 'PROMETHEUS_MULTIPROC_DIR:' /src/gen3-workflow-config.yaml | awk -F': ' '{print $2}' | tr -d '"')
+else
+  PROMETHEUS_MULTIPROC_DIR=""
+fi
 # Source the environment variables from the metrics setup script
-source "${CURRENT_DIR}/setup_prometheus"
+source "${CURRENT_DIR}/setup_prometheus" $PROMETHEUS_MULTIPROC_DIR
 
 echo "installing dependencies with 'poetry install -vv'..."
 poetry install -vv
