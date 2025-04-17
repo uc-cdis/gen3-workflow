2c6ba2c2da1d463a6f098564d89a0f62dd9e2aa6
Sai Shanmukha <nss10@outlook.com>
2025-04-17 10:18:32 -0500

    Fix unit test

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -6,7 +6,7 @@ from gen3workflow.config import config
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("endpoint", ["/", "/_status", "/ga4gh/tes/v1/tasks"])
-async def test_metrics_collection(monkeypatch, client, endpoint):
+async def test_metrics_collection(client, endpoint):
     """
     When metrics are enabled, metrics methods are called for endpoints present in `'/ga4gh/tes/v1/tasks'`.
     """
@@ -15,8 +15,9 @@ async def test_metrics_collection(monkeypatch, client, endpoint):
     with patch(
         "gen3workflow.metrics.Metrics.add_create_task_api_interaction"
     ) as mock_metrics:
-        res = await client.get(endpoint)
         if endpoint == "/ga4gh/tes/v1/tasks":
+            await client.post(endpoint, json={"name": "test_task"})
             mock_metrics.assert_called_once()
         else:
+            await client.get(endpoint)
             mock_metrics.assert_not_called()
