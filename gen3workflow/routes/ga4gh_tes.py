73cc3ae2fc1a0fc74a2de1643072f1996261546a
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2025-05-21 14:16:40 -0500

    funnel version 2025-05-19: pass auth header for plugin

--- gen3workflow/routes/ga4gh_tes.py
+++ gen3workflow/routes/ga4gh_tes.py
@@ -115,11 +115,13 @@ async def create_task(request: Request, auth=Depends(Auth)) -> dict:
         body["tags"] = {}
     # TODO unit test: user can't set USER_ID tag manually
     # TODO raise an error if it's set, and explain it's an internal tag. Same for AUTHZ tag
-    body["tags"]["USER_ID"] = user_id  # used by the funnel plugin to identify the user
+    # body["tags"]["USER_ID"] = user_id  # used by the funnel plugin to identify the user
     body["tags"]["AUTHZ"] = f"/users/{user_id}/gen3-workflow/tasks/TASK_ID_PLACEHOLDER"
 
     url = f"{config['TES_SERVER_URL']}/tasks"
-    res = await request.app.async_client.post(url, json=body)
+    res = await request.app.async_client.post(
+        url, json=body, headers={"Authorization": f"bearer {auth.bearer_token}"}
+    )
     if res.status_code != HTTP_200_OK:
         logger.error(f"TES server error at 'POST {url}': {res.status_code} {res.text}")
         raise HTTPException(res.status_code, res.text)
