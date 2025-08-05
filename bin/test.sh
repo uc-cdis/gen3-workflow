2ffe82531b61a1c1ea6fab71dc87904db60a3e60
Sai Shanmukha <nss10@outlook.com>
2025-08-05 12:44:36 -0500

    Move poetry install from common_setup and run it before _setup_db

--- bin/test.sh
+++ bin/test.sh
@@ -4,7 +4,13 @@ set -e
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "${CURRENT_DIR}/_common_setup.sh"
+
+echo "installing dependencies with 'poetry install -vv'..."
+poetry install -vv
+poetry env info
+
 source "${CURRENT_DIR}/_setup_db.sh"
+
 
 echo "running tests with 'pytest'..."
 poetry run pytest -vv --cov=gen3workflow --cov=migrations/versions --cov-report term-missing:skip-covered --cov-report xml
