9a1b7ca0c4b7e0feb8662db6032f6f1d4304a55c
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2026-05-04 15:44:33 -0500

    remove pins

--- .github/workflows/integration_tests_on_kind/ci_expose_kind_cluster.sh
+++ .github/workflows/integration_tests_on_kind/ci_expose_kind_cluster.sh
@@ -3,7 +3,11 @@ set -Eeuo pipefail
 
 # Expose the Kind cluster at http://localhost:8000
 kubectl port-forward -n "${NAMESPACE}" service/revproxy-service 8000:80 &
+
+# This ensures the port-forward command keeps running after the script exits, so the cluster
+# stays accessible
 disown $!
+
 # Wait until the port is actually accepting connections
 timeout 30 bash -c 'until nc -z localhost 8000; do sleep 0.5; done'
 
