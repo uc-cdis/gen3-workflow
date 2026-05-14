c07c15c8ddeda5a28c60aa2b2eb45c6d06eadab1
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2026-05-14 12:03:54 -0500

    validate token with TES aud

--- gen3workflow/auth.py
+++ gen3workflow/auth.py
@@ -58,9 +58,9 @@ class Auth:
             )
 
         try:
-            token_claims = await access_token("user", "openid", purpose="access")(
-                self.bearer_token
-            )
+            token_claims = await access_token(
+                "user", "openid", audience=["Gen3", "TES"], purpose="access"
+            )(self.bearer_token)
         except Exception as e:
             err_msg = "Could not verify, parse, and/or validate provided access token"
             logger.error(
