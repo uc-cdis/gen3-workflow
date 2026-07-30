7f0a7dba44801bcb5f23ce88190ffe7dc433b39a
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2026-07-30 14:33:25 -0500

    Update docstring

--- gen3workflow/aws/bucket.py
+++ gen3workflow/aws/bucket.py
@@ -421,8 +421,10 @@ async def create_user_bucket(user_id: str) -> Tuple[str, str, str]:
     Wrapper for `_create_user_bucket` that handles caching and retries.
 
     Gracefully handles race conditions, for example:
-    `An error occurred (OperationAborted) when calling the PutBucketEncryption operation:
-    A conflicting conditional operation is currently in progress against this resource.`
+    - `An error occurred (OperationAborted) when calling the PutBucketEncryption operation:
+      A conflicting conditional operation is currently in progress against this resource.`
+    - `An error occurred (AlreadyExistsException) when calling the CreateAlias operation:
+      An alias with the name XYZ already exists`
     """
     if USER_BUCKET_CACHE.has(user_id):
         return USER_BUCKET_CACHE.get(user_id)
