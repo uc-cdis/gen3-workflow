86a1ef59ae5ebde4556e6675eb10157cda5edfd6
Sai Shanmukha <nss10@outlook.com>
2026-09-01 15:34:51 -0500

    Remove uvloop to see if it shows any difference in performance

--- gen3workflow/app.py
+++ gen3workflow/app.py
@@ -79,6 +79,7 @@ def get_app(httpx_client=None) -> FastAPI:
     app.include_router(storage_router, tags=["Storage"])
     app.include_router(system_router, tags=["System"])
     app.include_router(ui_router, tags=["UI"])
+
     # Following will update logger level, propagate, and handlers
     get_logger("gen3workflow", log_level=log_level)
 
