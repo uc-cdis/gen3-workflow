4bab9107446497bb66a46ddb617b9e3aa8e6b02d
Sai Shanmukha <nss10@outlook.com>
2026-09-13 19:11:14 -0500

    disable netPolicy

--- .github/workflows/integration_tests_on_kind/ci_override_config_and_start_minio.sh
+++ .github/workflows/integration_tests_on_kind/ci_override_config_and_start_minio.sh
@@ -24,7 +24,7 @@ yq eval -i '.gen3-workflow.gen3WorkflowConfig.enableOptimizedNodeScheduling = fa
 
 # overwrite gen3-workflow config `EKS_CLUSTER_NAME` to an empty string
 yq eval -i '.global.clusterName = ""' values.yaml
-yq eval -i '.global.netPolicy.enabled = true' values.yaml
+yq eval -i '.global.netPolicy.enabled = false' values.yaml
 yq eval -i '.global.netPolicy.dbSubnets = []' values.yaml
 
 # update fence and indexd configs to generate secrets instead of looking for pre-existing secrets.
