f0020e30e4a3776dda96c988432db05e8c35908f
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2026-09-25 17:05:18 -0500

    Update minio image (#196)

--- .github/workflows/integration_tests_on_kind/ci_override_config_and_start_minio.sh
+++ .github/workflows/integration_tests_on_kind/ci_override_config_and_start_minio.sh
@@ -145,7 +145,7 @@ metadata:
 spec:
   containers:
     - name: minio
-      image: quay.io/minio/minio:latest-cicd
+      image: quay.io/cdis/minio:chainguard-latest-2026-09-25
       args:
         - server
         - /data
